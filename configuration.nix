2b2e5de6a69ff59a390479a619ac0b2117c6a4a3
ozzy330 <oskar.quesada30@gmail.com>
2024-02-26 18:58:39 -0600

    Add home-manager

--- configuration.nix
+++ configuration.nix
@@ -100,6 +100,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    home-manager
     nix-index
     htop
     wget
